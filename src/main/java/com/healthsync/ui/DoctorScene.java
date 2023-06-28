3493990836a9cccbe125b50fd9ac93753481d526
Luis-Medina12 <luis.medina0312@gmail.com>
2023-06-28 00:05:30 -0700

    Some styling completed

--- src/main/java/com/healthsync/ui/DoctorScene.java
+++ src/main/java/com/healthsync/ui/DoctorScene.java
@@ -20,6 +20,24 @@ public class DoctorScene extends BaseScene {
         content.setSpacing(30);
         content.setPadding(new Insets(20, 40, 20, 40));
 
+        String BorderLayout = """
+                -fx-border-color: black;
+                -fx-border-insets: 5;
+                -fx-border-width: 1;
+                -fx-border-radius: 30;
+                -fx-border-style: solid;
+                """;
+
+        String labelLayout = """
+                -fx-background-color: #7198E7;
+                -fx-background-radius: 30 30 0 0;
+                -fx-border-color: #1F2B6C;
+                -fx-border-width: 1;
+                -fx-border-radius: 30 30 0 0;
+                -fx-border-style: solid;
+                -fx-font-size: 15px;
+                -fx-font-weight: bold;
+                """;
 
         // need 3 boxes: Physical Test Findings, Prescription Entry, and Patient History
 
@@ -27,9 +45,13 @@ public class DoctorScene extends BaseScene {
         Label prescriptionEntryLabel = new Label("Prescription Entry");
         Label patientHistoryLabel = new Label("Patient History");
 
-        physicalTestFindingsLabel.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
-        prescriptionEntryLabel.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
-        patientHistoryLabel.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
+        physicalTestFindingsLabel.setAlignment(Pos.CENTER);
+        prescriptionEntryLabel.setAlignment(Pos.CENTER);
+        patientHistoryLabel.setAlignment(Pos.CENTER);
+
+        physicalTestFindingsLabel.setStyle(labelLayout);
+        prescriptionEntryLabel.setStyle(labelLayout);
+        patientHistoryLabel.setStyle(labelLayout);
 
         GridPane mainStructure = new GridPane();
         mainStructure.setVgap(15);
@@ -40,6 +62,19 @@ public class DoctorScene extends BaseScene {
         GridPane physicalTestFindings = new GridPane();
         GridPane prescriptionEntry = new GridPane();
         GridPane patientHistory = new GridPane();
+
+        physicalTestFindings.setPadding(new Insets(0,0,0,0));
+        physicalTestFindings.setPrefSize(800,300);
+
+        prescriptionEntry.setPadding(new Insets(0,0,0,0));
+        prescriptionEntry.setPrefSize(400,300);
+
+        patientHistory.setPadding(new Insets(0,0,0,0));
+        patientHistory.setPrefSize(1200,300);
+
+        physicalTestFindings.setStyle(BorderLayout);
+        prescriptionEntry.setStyle(BorderLayout);
+        patientHistory.setStyle(BorderLayout);
 
         physicalTestFindings.add(physicalTestFindingsLabel,0,0);
         prescriptionEntry.add(prescriptionEntryLabel,0,0);
