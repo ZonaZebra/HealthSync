31e13e051e7cc0a60c5bb0ba419f05b18a93ffd2
Luis-Medina12 <luis.medina0312@gmail.com>
2023-06-29 21:33:48 -0700

    Added method to specifically get phone number

--- src/main/java/com/healthsync/entities/Patient.java
+++ src/main/java/com/healthsync/entities/Patient.java
@@ -50,6 +50,18 @@ public class Patient extends User {
         this.pharmacyInformation = pharmacyInformation;
     }
 
+    public String getPhoneNumber(){
+        String phone= "";
+        int i =0;
+        while(contactInformation.charAt(i) != ','){
+            phone+=contactInformation.charAt(i);
+            i++;
+
+        }
+
+        return phone;
+    }
+
     @Override
     public String toString() {
         return "Patient{" +
