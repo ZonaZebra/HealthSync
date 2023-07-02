20709f85b3af8e97f6a7cf9e6ef60351cf6f4f30
Luis-Medina12 <luis.medina0312@gmail.com>
2023-07-01 23:12:08 -0700

    Working towards creating unique IDs for findings

--- src/main/java/com/healthsync/dao/PrescriptionsDao.java
+++ src/main/java/com/healthsync/dao/PrescriptionsDao.java
@@ -5,6 +5,7 @@ import com.healthsync.util.DBConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -17,15 +18,16 @@ public class PrescriptionsDao {
                 return false;
             }
 
-            String sql = "INSERT INTO prescriptions (prescription_id, product, dosage_in_mg, frequency, instructions, pharmacy_id, prescribed_by) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            String sql = "INSERT INTO prescriptions (prescription_id, product, dosage_in_mg, frequency, instructions, pharmacy_id, patient_id, prescribed_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement stmt = conn.prepareStatement(sql);
             stmt.setInt(1, prescription.getPrescription_id());
             stmt.setString(2, prescription.getProduct());
             stmt.setInt(3, prescription.getDosage_in_mg());
             stmt.setInt(4, prescription.getFrequency());
             stmt.setString(5, prescription.getInstructions());
-            stmt.setInt(6, prescription.getPharmacy_id());
-            stmt.setString(7, prescription.getPrescribed_by());
+            stmt.setString(6, prescription.getPharmacy_id());
+            stmt.setString(7, prescription.getPatient_id());
+            stmt.setString(8, prescription.getPrescribed_by());
 
             int rowsInserted = stmt.executeUpdate();
             return rowsInserted > 0;
@@ -59,4 +61,35 @@ public class PrescriptionsDao {
         }
         return false;
     }
+
+    public Prescriptions getPrescription(String patientID) {
+        try (Connection conn = DBConnection.getConnection()) {
+            if (conn == null) {
+                System.err.println("Failed to establish database connection.");
+                return null;
+            }
+
+            String sql = "SELECT * FROM prescriptions WHERE patient_id = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, patientID);
+
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                int testID = rs.getInt("prescription_id");
+                String product= rs.getString("product");
+                int dosage= rs.getInt("dosage_in_mg");
+                int frequency= rs.getInt("frequency");
+                String instruction= rs.getString("instructions");
+                String pharmaID= rs.getString("pharmacy_id");
+                String patient_ID= rs.getString("patient_id");
+                String prescriber= rs.getString("prescribed_by");
+
+                return new Prescriptions(testID,product,dosage,frequency, instruction,pharmaID,patient_ID,prescriber);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
--- src/main/java/com/healthsync/service/DoctorService.java
+++ src/main/java/com/healthsync/service/DoctorService.java
@@ -1,4 +1,14 @@
 package com.healthsync.service;
 
+import com.healthsync.dao.PatientDao;
+import com.healthsync.dao.PhysicalTestFindingsDao;
+import com.healthsync.dao.PrescriptionsDao;
+
 public class DoctorService {
+    private final PatientDao patientDao = new PatientDao();
+    private final PhysicalTestFindingsDao testFindingsDao= new PhysicalTestFindingsDao();
+    private final PrescriptionsDao prescriptionsDao = new PrescriptionsDao();
+
+
+
 }
