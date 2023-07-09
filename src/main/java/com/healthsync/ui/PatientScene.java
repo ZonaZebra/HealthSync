0f5e1c854e859c72402b6145115f930d13d4826d
Luis-Medina12 <luis.medina0312@gmail.com>
2023-07-09 01:29:39 -0700

    Updated how patient UI displays to match other UIs

--- src/main/java/com/healthsync/ui/PatientScene.java
+++ src/main/java/com/healthsync/ui/PatientScene.java
@@ -8,25 +8,18 @@ import com.healthsync.entities.Appointments;
 import com.healthsync.entities.Messages;
 import com.healthsync.entities.Patient;
 import com.healthsync.entities.User;
-import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.Stack;
 
 public class PatientScene extends BaseScene {
     private static final PatientDao patientDao = new PatientDao();
@@ -42,25 +35,35 @@ public class PatientScene extends BaseScene {
         Patient patient = patientDao.getPatientById(user.getUserId());
         DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
 
+        VBox mainContainer = new VBox(20);
+        HBox contentContainer = new HBox(20);
+        contentContainer.setAlignment(Pos.CENTER);
+        contentContainer.setPrefSize(1200,400);
+
+        VBox patientInfoContainer = new VBox();
+
+        VBox AppointmentsContainer = new VBox();
+
+        VBox MessagesContainer = new VBox();
+
         BorderPane content = new BorderPane();
 
         Label title = new Label("Patient Portal");
         title.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
         StackPane titleBox = new StackPane(title);
-        content.setTop(titleBox);
+//        content.setTop(titleBox);
+        mainContainer.getChildren().add(titleBox);
+
 
 
 //----------------------Patient Information Panel----------------------//
-        HBox LeftPanel = new HBox();
-        content.setLeft(LeftPanel);
-
         VBox PatientInformation = new VBox();
-        PatientInformation.setPrefWidth(250);
-        PatientInformation.setMinWidth(250);
+        PatientInformation.setPrefWidth(400);
+        PatientInformation.setMinHeight(400);
         PatientInformation.setStyle("-fx-background-color:white;");
 
         Label ptInfo = new Label("Patient Information");
-        ptInfo.setStyle("-fx-font-size: 14px; -fx-font-weight: bolder");
+        ptInfo.setStyle("-fx-font-size: 20px; -fx-font-weight: bolder");
         HBox ptInfoBox = new HBox(ptInfo);
         ptInfoBox.setStyle("-fx-background-color:#7C8EB0");
         //PatientInformation.getChildren().add(ptInfoBox);
@@ -70,10 +73,10 @@ public class PatientScene extends BaseScene {
 
         //Name Field
         Label Name = new Label("Name");
-        Name.setStyle("-fx-font-size: 12px; -fx-font-weight: bold");
+        Name.setStyle("-fx-font-size: 16px; -fx-font-weight: bold");
         Label ptName = new Label(patient.getFirstName() + " " + patient.getLastName());
         PatientInformation.getChildren().add(Name);
-        ptName.setStyle("-fx-font-size: 11px");
+        ptName.setStyle("-fx-font-size: 14px");
         HBox ptNameBox = new HBox(ptName);
         PatientInformation.getChildren().add(ptNameBox);
         ptNameBox.setPadding(new Insets(0, 0, 10, 0));
@@ -81,11 +84,11 @@ public class PatientScene extends BaseScene {
 
         //DOB Field
         Label DOB = new Label("Date of Birth");
-        DOB.setStyle("-fx-font-size: 12px; -fx-font-weight: bold");
+        DOB.setStyle("-fx-font-size: 16px; -fx-font-weight: bold");
         PatientInformation.getChildren().add(DOB);
         String ptbday = (dateFormat.format(patient.getBirthday())).toString();
         Label ptDOB = new Label(ptbday);
-        ptDOB.setStyle("-fx-font-size: 11px");
+        ptDOB.setStyle("-fx-font-size: 14px");
         HBox ptDOBBox = new HBox(ptDOB);
         PatientInformation.getChildren().add(ptDOBBox);
         ptDOBBox.setPadding(new Insets(0, 0, 10, 0));
@@ -93,11 +96,11 @@ public class PatientScene extends BaseScene {
 
         //Phone Field
         Label Phone = new Label("Phone Number");
-        Phone.setStyle("-fx-font-size: 12px; -fx-font-weight: bold");
+        Phone.setStyle("-fx-font-size: 16px; -fx-font-weight: bold");
         PatientInformation.getChildren().add(Phone);
         String ptphone = patient.getContactInformation().toString().substring(0, 9);
         Label ptPhone = new Label(ptphone);
-        ptPhone.setStyle("-fx-font-size: 11px");
+        ptPhone.setStyle("-fx-font-size: 14px");
         HBox ptPhoneBox = new HBox(ptPhone);
         PatientInformation.getChildren().add(ptPhoneBox);
         ptPhoneBox.setPadding(new Insets(0, 0, 10, 0));
@@ -105,11 +108,11 @@ public class PatientScene extends BaseScene {
 
         //Email Field
         Label Email = new Label("Email Address");
-        Email.setStyle("-fx-font-size: 12px; -fx-font-weight: bold");
+        Email.setStyle("-fx-font-size: 16px; -fx-font-weight: bold");
         PatientInformation.getChildren().add(Email);
         String ptemail = patient.getContactInformation().substring(11, (patient.getContactInformation().toString().length()));
         Label ptEmail = new Label(ptemail);
-        ptEmail.setStyle("-fx-font-size: 11px");
+        ptEmail.setStyle("-fx-font-size: 14px");
         HBox ptEmailBox = new HBox(ptEmail);
         PatientInformation.getChildren().add(ptEmailBox);
         ptEmailBox.setPadding(new Insets(0, 0, 10, 0));
@@ -117,12 +120,12 @@ public class PatientScene extends BaseScene {
 
         //Insurance Field
         Label Insurance = new Label("Insurance");
-        Insurance.setStyle("-fx-font-size: 12px; -fx-font-weight: bold");
+        Insurance.setStyle("-fx-font-size: 16px; -fx-font-weight: bold");
         PatientInformation.getChildren().add(Insurance);
         String ptinsurance = patient.getInsuranceInformation().substring(0, (patient.getInsuranceInformation().indexOf(',')))
                 + "\n" + patient.getInsuranceInformation().substring((patient.getInsuranceInformation().indexOf(',') + 1), patient.getInsuranceInformation().length());
         Label ptInsurance = new Label(ptinsurance);
-        ptInsurance.setStyle("-fx-font-size: 11px");
+        ptInsurance.setStyle("-fx-font-size: 14px");
         HBox ptInsuranceBox = new HBox(ptInsurance);
         PatientInformation.getChildren().add(ptInsuranceBox);
         ptInsuranceBox.setPadding(new Insets(0, 0, 10, 0));
@@ -130,16 +133,19 @@ public class PatientScene extends BaseScene {
 
         //Pharmacy Field
         Label Pharmacy = new Label("Pharmacy");
-        Pharmacy.setStyle("-fx-font-size: 12px; -fx-font-weight: bold");
+        Pharmacy.setStyle("-fx-font-size: 16px; -fx-font-weight: bold");
         PatientInformation.getChildren().add(Pharmacy);
         String ptpharm = patient.getPharmacyInformation().substring(0, (patient.getPharmacyInformation().indexOf(',')))
                 + "\n" + patient.getPharmacyInformation().substring((patient.getPharmacyInformation().indexOf(',') + 1), patient.getPharmacyInformation().length());
         Label ptPharmacy = new Label(ptpharm);
-        ptPharmacy.setStyle("-fx-font-size: 11px");
+        ptPharmacy.setStyle("-fx-font-size: 14px");
         HBox ptPharmacyBox = new HBox(ptPharmacy);
         PatientInformation.getChildren().add(ptPharmacyBox);
         ptPharmacyBox.setPadding(new Insets(0, 0, 10, 0));
         ptPharmacyBox.setAlignment(Pos.BASELINE_RIGHT);
+
+        contentContainer.getChildren().add(LeftPanelContent);
+
 
 //        StackPane EditInfo = new StackPane();
 //        Button EditInfoButton = new Button("Edit");
@@ -200,39 +206,36 @@ public class PatientScene extends BaseScene {
 //        EditInfo.setAlignment(Pos.BOTTOM_RIGHT);
 //        PatientInformation.getChildren().add(EditInfo);
 
-        LeftPanel.getChildren().add(LeftPanelContent);
-
-        Rectangle spacer = new Rectangle();
-        spacer.setWidth(10);
-        LeftPanel.getChildren().add(spacer);
-
-
 //----------------------Appointments Panel----------------------//
-//                                    for (int i = 0; i < 4; i++){
-//                                        appointmentsDao.createAppointment(new Appointments(0,"TestPatient11111","TestDoctor11111", (new Date(2023, i, i+10)),0,0,0 ));
-//                                    }
+//        for (int i = 0; i < 4; i++){
+//            appointmentsDao.createAppointment(new Appointments(0,"TestPatient11111","TestDoctor11111",
+//                    (new Date(2023, i, i+10)),0,0,0 ));
+//        }
         List<Appointments> PTappts = appointmentsDao.getAppointmentsByPatientId(user.getUserId());
 
-
         // Panel Header
-        HBox CenterPanel = new HBox();
+        VBox CenterPanel = new VBox();
         VBox Appointments = new VBox();
+        Appointments.setPrefSize(400,400);
+        Appointments.setAlignment(Pos.TOP_CENTER);
+        Appointments.setPadding(new Insets(20,0,0,0));
         Appointments.setStyle("-fx-background-color:white");
-        content.setCenter(CenterPanel);
+//        content.setCenter(CenterPanel);
         Appointments.setMinWidth(350);
 
         Label appts = new Label("Appointments");
-        appts.setStyle("-fx-font-size: 14px; -fx-font-weight: bolder");
+        appts.setStyle("-fx-font-size: 20px; -fx-font-weight: bolder");
         HBox ptApptsBox = new HBox(appts);
         ptApptsBox.setStyle("-fx-background-color:#7C8EB0");
-        Appointments.getChildren().add(ptApptsBox);
+        CenterPanel.getChildren().add(ptApptsBox);
         ptApptsBox.setAlignment(Pos.CENTER);
 
         Label apptHist = new Label("Appointment History");
-        apptHist.setStyle("-fx-font-size: 14px; -fx-font-weight: bolder");
+        apptHist.setStyle("-fx-font-size: 16px; -fx-font-weight: bolder");
 
         if (PTappts.isEmpty()) {
             Label empty = new Label("No upcoming appointments.");
+            empty.setStyle("-fx-font-size: 14px;");
             Appointments.getChildren().add(empty);
             empty.setAlignment(Pos.TOP_CENTER);
         } else {
@@ -245,7 +248,8 @@ public class PatientScene extends BaseScene {
                 Label apptDate = new Label(apptDateString);
                 upcAppts.add(apptDate, 0, i);
 
-                String providerNameString = userDao.getUserById(PTappts.get(i).getDoctor_id()).getFirstName() + " " + userDao.getUserById(PTappts.get(i).getDoctor_id()).getLastName();
+                String providerNameString = userDao.getUserById(PTappts.get(i).getDoctor_id()).getFirstName() + " " +
+                        userDao.getUserById(PTappts.get(i).getDoctor_id()).getLastName();
                 Label providerName = new Label(providerNameString);
                 upcAppts.add(providerName, 1, i);
             }
@@ -262,34 +266,39 @@ public class PatientScene extends BaseScene {
         spacer2.setMaxWidth(10);
         spacer2.setMinWidth(10);
         spacer2.setPrefWidth(10);
-        CenterPanel.getChildren().add(spacer2);
+//        CenterPanel.getChildren().add(spacer2);
 
+        contentContainer.getChildren().add(CenterPanel);
 
 //----------------------Messages Panel----------------------//
-//                        for (int i = 0; i < 4; i++){
-//                            messagesDao.createMessage(new Messages (0, "TestPatient11111", "TestNurse11111", (new Date(2023, i, i+10)), "Subject of message " + (i+1), "This is the message."));
-//                        }
+//        for (int i = 0; i < 4; i++){
+//            messagesDao.createMessage(new Messages (0, "TestPatient11111", "TestNurse11111",
+//                    (new Date(2023, i, i+10)), "Subject of message " + (i+1), "This is the message."));
+//        }
 //
 //        for (int i = 0; i < 4; i++){
-//            messagesDao.createMessage(new Messages (0, "TestDoctor11111", "TestPatient11111", (new Date(2023, i, i+10)), "Subject of message " + (i+1), "This is the message."));
+//            messagesDao.createMessage(new Messages (0, "TestDoctor11111", "TestPatient11111",
+//                    (new Date(2023, i, i+10)), "Subject of message " + (i+1), "This is the message."));
 //        }
 
         List<Messages> PTMsgsIN = messagesDao.getMessagesByReceiverId(user.getUserId());
         List<Messages> PTMsgsOUT = messagesDao.getMessagesBySenderId(user.getUserId());
 
-        HBox RightPanel = new HBox();
-        VBox Messages = new VBox();
-        Messages.setMinWidth(500);
+        VBox RightPanel = new VBox();
+        RightPanel.setMinWidth(400);
+        VBox Messages = new VBox(10);
+        Messages.setPrefSize(400, 400);
         Messages.setStyle("-fx-background-color:white");
-        RightPanel.setAlignment(Pos.TOP_LEFT);
-        content.setRight(RightPanel);
+        Messages.setAlignment(Pos.TOP_CENTER);
+//        RightPanel.setAlignment(Pos.CENTER);
+//        content.setRight(RightPanel);
 
         Label msgs = new Label("Messages");
-        msgs.setStyle("-fx-font-size: 14px; -fx-font-weight: bolder");
+        msgs.setStyle("-fx-font-size: 20px; -fx-font-weight: bolder");
         HBox ptMsgsBox = new HBox(msgs);
         ptMsgsBox.setStyle("-fx-background-color:#7C8EB0");
-        Messages.getChildren().add(ptMsgsBox);
-        ptMsgsBox.setAlignment(Pos.CENTER_LEFT);
+        RightPanel.getChildren().add(ptMsgsBox);
+        ptMsgsBox.setAlignment(Pos.CENTER);
 
         // Inbox Section
         Label inbox = new Label("Inbox");
@@ -299,13 +308,13 @@ public class PatientScene extends BaseScene {
         if (PTMsgsIN.isEmpty()) {
             Label empty1 = new Label("No new messages.");
             Messages.getChildren().add(empty1);
-            empty1.setAlignment(Pos.CENTER);
+//            empty1.setAlignment(Pos.CENTER);
         } else {
             GridPane msgsIN = new GridPane();
             msgsIN.setPadding(new Insets(0, 0, 0, 20));
             msgsIN.setVgap(10);
             msgsIN.setHgap(20);
-            msgsIN.setAlignment(Pos.CENTER_LEFT);
+//            msgsIN.setAlignment(Pos.CENTER_LEFT);
             for (int i = 0; i < PTMsgsIN.size(); i++) {
                 String msgINDateString = (PTMsgsIN.get(i).getDateSent().toString());
                 Label msgINDate = new Label(msgINDateString);
@@ -315,7 +324,8 @@ public class PatientScene extends BaseScene {
                 Label msgINsubject = new Label(msgINsubjectString);
                 msgsIN.add(msgINsubject, 1, i);
 
-                String msgINfromString = userDao.getUserById(PTMsgsIN.get(i).getSenderID()).getFirstName() + " " + userDao.getUserById(PTMsgsIN.get(i).getSenderID()).getLastName();
+                String msgINfromString = userDao.getUserById(PTMsgsIN.get(i).getSenderID()).getFirstName() + " " +
+                        userDao.getUserById(PTMsgsIN.get(i).getSenderID()).getLastName();
                 Label msgINfrom = new Label(msgINfromString);
                 msgsIN.add(msgINfrom, 2, i);
 
@@ -325,19 +335,19 @@ public class PatientScene extends BaseScene {
 
         // Sent Section
         Label sent = new Label("Sent");
-        inbox.setStyle("-fx-font-size: 14px; -fx-font-weight: bolder");
+        sent.setStyle("-fx-font-size: 14px; -fx-font-weight: bolder");
         Messages.getChildren().add(sent);
 
         if (PTMsgsOUT.isEmpty()) {
             Label empty2 = new Label("No messages sent.");
             Messages.getChildren().add(empty2);
-            empty2.setAlignment(Pos.CENTER);
+//            empty2.setAlignment(Pos.CENTER);
         } else {
             GridPane msgsOUT = new GridPane();
             msgsOUT.setPadding(new Insets(0, 0, 0, 20));
             msgsOUT.setVgap(10);
             msgsOUT.setHgap(20);
-            msgsOUT.setAlignment(Pos.CENTER_LEFT);
+//            msgsOUT.setAlignment(Pos.CENTER_LEFT);
             for (int i = 0; i < PTMsgsOUT.size(); i++) {
                 String msgODateString = (PTMsgsOUT.get(i).getDateSent().toString());
                 Label msgODate = new Label(msgODateString);
@@ -347,7 +357,8 @@ public class PatientScene extends BaseScene {
                 Label msg0subject = new Label(msg0subjectString);
                 msgsOUT.add(msg0subject, 1, i);
 
-                String msgOtoString = userDao.getUserById(PTMsgsOUT.get(i).getReceiverID()).getFirstName() + " " + userDao.getUserById(PTMsgsOUT.get(i).getReceiverID()).getLastName();
+                String msgOtoString = userDao.getUserById(PTMsgsOUT.get(i).getReceiverID()).getFirstName() + " " +
+                        userDao.getUserById(PTMsgsOUT.get(i).getReceiverID()).getLastName();
                 Label msgOto = new Label(msgOtoString);
                 msgsOUT.add(msgOto, 2, i);
             }
@@ -355,6 +366,7 @@ public class PatientScene extends BaseScene {
         }
         StackPane SendMess = new StackPane();
         Button SendMessButton = new Button("Send New Message");
+        SendMessButton.setStyle("-fx-font-size: 14px; -fx-background-radius: 15;");
         SendMessButton.setOnAction(event -> {
             Stage popupwindow = new Stage();
             popupwindow.setMinWidth(650);
@@ -364,7 +376,7 @@ public class PatientScene extends BaseScene {
             GridPane form = new GridPane();
             form.setVgap(10);
             form.setHgap(20);
-            form.setAlignment(Pos.CENTER);
+//            form.setAlignment(Pos.CENTER);
 
             Label To = new Label("Recipient ID: ");
             TextField ToField = new TextField();
@@ -387,7 +399,7 @@ public class PatientScene extends BaseScene {
             form.add(BodyField, 1, 2);
 
             Button Send = new Button("Send");
-            Send.setAlignment(Pos.CENTER_RIGHT);
+//            Send.setAlignment(Pos.CENTER_RIGHT);
             Send.setOnAction(e -> {
                 messagesDao.createMessage(new Messages(
                         0,
@@ -407,7 +419,7 @@ public class PatientScene extends BaseScene {
             layout.setPadding(new Insets(0, 20, 0, 20));
             layout.getChildren().addAll(form, Send);
 
-            layout.setAlignment(Pos.CENTER);
+//            layout.setAlignment(Pos.CENTER);
 
             Scene scene1 = new Scene(layout, 300, 450);
             popupwindow.setScene(scene1);
@@ -415,11 +427,14 @@ public class PatientScene extends BaseScene {
         });
 
         Messages.getChildren().add(SendMessButton);
-        SendMessButton.setAlignment(Pos.BOTTOM_RIGHT);
+//        SendMessButton.setAlignment(Pos.BOTTOM_RIGHT);
         RightPanel.getChildren().add(Messages);
 
-        BorderPane.setAlignment(content, Pos.TOP_CENTER);
+        contentContainer.getChildren().add(RightPanel);
+        mainContainer.getChildren().add(contentContainer);
 
-        return content;
+//        BorderPane.setAlignment(content, Pos.TOP_CENTER);
+
+        return mainContainer;
     }
 }
